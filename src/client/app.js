960e2d6131c9f63be840b3abc3a688b103021671
Jorn Bostelaar <bostelaarj@gmail.com>
2016-06-29 09:19:42 +0200

    router: updated to BrowserHistory

--- src/client/app.js
+++ src/client/app.js
@@ -1,11 +1,10 @@
 import App from 'client/components/App';
-import history from 'client/utils/history';
 import React from 'react';
 import { render } from 'react-dom';
-import { Router, Route } from 'react-router';
+import { Router, Route, browserHistory } from 'react-router';
 
 render((
-	<Router history={history}>
+	<Router history={browserHistory}>
 		<Route path="/" component={App} />
 	</Router>
 ), document.getElementById('app'));
