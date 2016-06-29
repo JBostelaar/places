b6ab276b1b982ad63980c8e2264fd5630a0bae7f
Jorn Bostelaar <bostelaarj@gmail.com>
2016-06-29 12:10:37 +0200

    server: import store

--- src/server/index.js
+++ src/server/index.js
@@ -1,21 +1,17 @@
-import * as appReducers from 'app/reducers';
+import store from 'app/store';
+import getRoutes from 'app/utils/getRoutes';
 import express from 'express';
 import React from 'react';
 import { renderToString } from 'react-dom/server';
 import { Provider } from 'react-redux';
 import { match, RouterContext } from 'react-router';
-import { combineReducers, createStore } from 'redux';
 import renderFullPage from 'server/utils/renderFullPage';
-import getRoutes from 'app/utils/getRoutes';
 
 const app = express();
 
 app.use(express.static('dist/client'));
 
 app.use((req, res, next) => {
-	const store = createStore(combineReducers({
-		...appReducers,
-	}));
 	const routes = getRoutes();
 
 	match({ routes, location: req.url }, (error, redirectLocation, renderProps) => {
