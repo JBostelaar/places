501f6c8924cb79ac66a8fc61c65111894565687a
Jorn Bostelaar <bostelaarj@gmail.com>
2016-08-09 21:47:23 +0200

    gulp: added svg sprites

--- gulp/index.js
+++ gulp/index.js
@@ -4,7 +4,7 @@ const gutil = require('gulp-util');
 
 globby.sync(`${__dirname}/tasks/*.js`).forEach(task => require(task));
 
-const tasks = ['babel', 'scripts', 'styles', 'symlink', 'images'];
+const tasks = ['babel', 'scripts', 'styles', 'symlink', 'images', 'svg'];
 
 if (gutil.env.dev) {
 	tasks.push('nodemon', 'watch');
