374b57db8a68ffb0c5cf9393d24254e0d013bacf
Jorn Bostelaar <bostelaarj@gmail.com>
2016-08-09 21:57:16 +0200

    gulp: rewrote css task

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,12 +1,41 @@
+/* eslint-disable global-require */
 const gulp = require('gulp');
-const sass = require('gulp-sass');
-const cssnano = require('gulp-cssnano');
-const sourcemaps = require('gulp-sourcemaps');
+const gutil = require('gulp-util');
+const c = gutil.colors;
+
+function run(src) {
+	const sourcemaps = require('gulp-sourcemaps');
+
+	gutil.log(`${c.cyan('styles')}: compiling`);
+	return gulp.src(src)
+		.pipe(sourcemaps.init())
+		.pipe(require('gulp-sass')())
+		.on('error', function handleError(err) {
+			gutil.log(`${c.cyan('styles')}: ${c.red('an error occured')}`);
+			console.error(err.message);
+			this.emit('end');
+		})
+		.pipe(require('gulp-autoprefixer')({
+			browsers: ['last 2 versions'],
+		}))
+		.pipe(gutil.env.dev ? gutil.noop() : require('gulp-cssnano')({
+			discardUnused: { fontFace: false },
+		}))
+		.pipe(sourcemaps.write('.'))
+		.pipe(gulp.dest('dist/public/assets/css'))
+		.on('finish', () => {
+			gutil.log(`${c.cyan('styles')}: done`);
+		});
+}
 
 gulp.task('styles', () => {
-	gulp.src('./src/app/styles/*.scss')
-		.pipe(sass().on('error', sass.logError))
-		.pipe(cssnano())
-		.pipe(sourcemaps.write('.'))
-		.pipe(gulp.dest('./dist/client'));
+	const src = 'src/app/styles/*.scss';
+	if (gutil.env.dev) {
+		gutil.log(`${c.cyan('styles')}: watching`);
+		gulp.watch([
+			'src/app/styles/*.scss',
+			'src/app/styles/**/*.scss',
+		], () => run(src));
+	}
+	return run(src);
 });
